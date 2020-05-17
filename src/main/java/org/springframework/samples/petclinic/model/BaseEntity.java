a082f7988ca96d69fe7225b18203916e19c378d3
Kanstantsin_Melnikau <Kanstantsin_Melnikau@epam.com>
2020-05-17 15:26:09 +0300

    Test commit

--- src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
+++ src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
@@ -35,6 +35,7 @@ public class BaseEntity implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	//KM was here
 
 	public Integer getId() {
 		return id;
